6dc6f2d2ac667cd725e83e336a7e130494817830
Black-hawk-0020 <valentyn0020@gmail.com>
2026-02-08 17:51:43 +0200

    add hw1

---
+++ index.js
@@ -0,0 +1,46 @@
+// dYQNrBV
+// let a = "hello ";
+// let b = "owu ";
+// let c = "com ";
+// let d = "ua";
+// let num1 = 1;
+// let num2 = 10;
+// let num3 = -999;
+// let num4 = 123;
+// let num5 = 3.14;
+// let num6 = 2.7;
+// let num7 = 16;
+// let bul1 = true;
+// let bul2 = false;
+// console.log(a + b + c + d);
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+// console.log(num4);
+// console.log(num5);
+// console.log(num6);
+// console.log(num7);
+// console.log(bul1);
+// console.log(bul2);
+
+// 6Qb97gsv
+// let firstName = "Валентин ";
+// let middleName = "Васильович";
+// let lastName = "Маслов ";
+// let person = lastName + firstName + middleName;
+// console.log(person);
+
+//4N0y5tufA
+// let a = 100;
+// let b = "100";
+// let c = true;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+//ruUtWDUI
+let lastName = prompt("Введіть ваше прізвище");
+let firstName = prompt("Введіть ваше ім'я");
+let middleName = prompt("Введіть ім'я по батькові");
+let age = prompt("Введіть ваш вік");
+console.log(lastName+" "+firstName+" "+middleName+" вік: "+age+" років")
