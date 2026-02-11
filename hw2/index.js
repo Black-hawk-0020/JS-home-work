411083d4b63e68516999596a49cd3ce881af6ec8
Black-hawk-0020 <valentyn0020@gmail.com>
2026-02-11 15:23:53 +0200

    add hw2

---
+++ hw2/index.js
@@ -0,0 +1,269 @@
+//67kfznmiMl
+// let arr = [
+//   "html",
+//   "css",
+//   "js",
+//   "mysql",
+//   "mongodb",
+//   "react",
+//   "angular",
+//   "aws",
+//   "docker",
+//   "git",
+// ];
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+// console.log(arr[5]);
+// console.log(arr[6]);
+// console.log(arr[7]);
+// console.log(arr[8]);
+// console.log(arr[9]);
+
+//LARqoUj5I
+// let book1 = {
+//   title: "Голодні ігри",
+//   pageCount: 200,
+//   genre: "художній",
+// };
+// let book2={
+//     title:'Гра престолів',
+//     pageCount:278,
+//     genre:'художній'
+// }
+// let book3={
+//     title:'Дюна',
+//     pageCount:150,
+//     genre:'роман'
+// }
+
+//sA3Gg1sCp
+// let book1 = {
+//   title: "Граф Монте-Крісто",
+//   pageCount: 250,
+//   genre: "художній",
+//   authors: [
+//     {
+//       name: "Александр Дюма ",
+//       age: 68,
+//     },
+//   ],
+// };
+// let book2 = {
+//   title: "Гра престолів",
+//   pageCount: 278,
+//   genre: "художній",
+//   authors: [
+//     {
+//       name: "Александр Дюма ",
+//       age: 68,
+//     },
+//   ],
+// };
+// let book3 = {
+//   title: "Дюна",
+//   pageCount: 150,
+//   genre: "роман",
+//   authors: [
+//     {
+//       name: "Френк Герберт ",
+//       age: 65,
+//     },
+//   ],
+// };
+
+//jCHFnEbdmFd
+// let users = [
+//   {
+//     name: "Leanne Graham",
+//     username: "Bret",
+//     password: 12345678,
+//   },
+//   {
+//     name: "Ervin Howell",
+//     username: "Antonette",
+//     password: 14865792,
+//   },
+//   {
+//     name: "Clementine Bauch",
+//     username: "Samantha",
+//     password: 87496857,
+//   },
+//   {
+//     name: "Patricia Lebsack",
+//     username: "Karianne",
+//     password: 78412561,
+//   },
+//   {
+//     name: "Chelsey Dietrich",
+//     username: "Kamren",
+//     password: 48971256,
+//   },
+//   {
+//     name: "Mrs. Dennis Schulist",
+//     username: "Leopoldo_Corkery",
+//     password: 80157963,
+//   },
+//   {
+//     name: "Kurtis Weissnat",
+//     username: "Elwyn.Skiles",
+//     password: 84905601,
+//   },
+//   {
+//     name: "Nicholas Runolfsdottir V",
+//     username: "Maxime_Nienow",
+//     password: 10504893,
+//   },
+//   {
+//     id: 9,
+//     name: "Glenna Reichert",
+//     username: "Delphine",
+//     password: 48631097,
+//   },
+//   {
+//     id: 10,
+//     name: "Clementina DuBuque",
+//     username: "Moriah.Stanton",
+//     password: 34087067,
+//   },
+// ];
+// console.log(users[0].password);
+// console.log(users[1].password);
+// console.log(users[2].password);
+// console.log(users[3].password);
+// console.log(users[4].password);
+// console.log(users[5].password);
+// console.log(users[6].password);
+// console.log(users[7].password);
+// console.log(users[9].password);
+// console.log(users[9].password);
+// console.log(users[9].password);
+
+//coYydZuaeEB
+// let monday = {
+//   morning: 10,
+//   afternoon: 20,
+//   evening: 15,
+// };
+// let tuesday = {
+//   morning: 8,
+//   afternoon: 20,
+//   evening: 14,
+// };
+// let wednesday = {
+//   morning: 10,
+//   afternoon: 25,
+//   evening: 15,
+// };
+// let thursday = {
+//   morning: 10,
+//   afternoon: 20,
+//   evening: 13,
+// };
+// let friday = {
+//   morning: 10,
+//   afternoon: 20,
+//   evening: 16,
+// };
+// let saturday = {
+//   morning: 10,
+//   afternoon: 20,
+//   evening: 15,
+// };
+// let sunday = {
+//   morning: 11,
+//   afternoon: 20,
+//   evening: 15,
+// };
+// let arrTemp = [];
+// arrTemp.push(monday);
+// arrTemp.push(tuesday);
+// arrTemp.push(wednesday);
+// arrTemp.push(thursday);
+// arrTemp.push(friday);
+// arrTemp.push(saturday);
+// arrTemp.push(sunday);
+// console.log(arrTemp);
+
+//bAUsaq6LI
+// let x = 0;
+// console.log(x !== 0 ? "вірно" : "невірно");
+
+//3ckURgvs
+// let time = 0;
+// if (time >= 0 && time < 15) {
+//   console.log("1 четверть");
+// } else if (time >= 15 && time < 30) {
+//   console.log("2 четверть");
+// } else if (time >= 30 && time < 45) {
+//   console.log("3 четверть");
+// } else console.log("4 четверть");
+
+//UMoNq4biWGe
+// let day = 30;
+// if (day >= 1 && day <= 10) {
+//   console.log("1 декада");
+// } else if (day >= 11 && day <= 20) {
+//   console.log("2 декада");
+// } else console.log("3 декада");
+
+//KzrtqyQ
+// let numDay = +prompt("Введіть номер дня тижня");
+// switch (numDay) {
+//   case 1:
+//     console.log("monday");
+//     break;
+//   case 2:
+//     console.log("tuesday");
+//     break;
+//   case 3:
+//     console.log("wednesday");
+//     break;
+//   case 4:
+//     console.log("thursday");
+//     break;
+//   case 5:
+//     console.log("friday");
+//     break;
+//   case 6:
+//     console.log("saturday");
+//     break;
+//   case 7:
+//     console.log("sunday");
+//     break;
+//   default:
+//     break;
+// }
+
+//uwsz1RnTQJ1
+// let x = 15;
+// let y = 16;
+// if (x > y) {
+//   console.log(x + " більше");
+// } else if (y > x) {
+//   console.log(y + " більше");
+// } else console.log("Числа рівні");
+
+//iBvqtjEm
+// let x = null;
+// if (!!x === false) {
+//   x = "default";
+// }
+// console.log(x);
+
+//#awLXL6TBzg
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+for (const element of coursesAndDurationArray) {
+    if (element.monthDuration>5) {
+        console.log(element.title+' Супер')
+    }
+}
